722f3f53aabdc9820e1ca882677e023556eba78e
leonardo.oliveira <eng_leonardokale@outlook.com>
2024-09-07 16:15:58 -0300

    add theme

--- snackhub_react/src/routes/index.tsx
+++ snackhub_react/src/routes/index.tsx
@@ -1,11 +1,21 @@
 import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export const AppRoutes = () => {
 
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Olá mundo</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Olá mundo
+          </Button>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
